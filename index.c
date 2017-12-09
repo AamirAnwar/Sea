9fa71b39c540b551385a0eee522c03bd07cd9617
Aamir Anwar <aamir.anwar021193@gmail.com>
2017-12-09 21:53:00 +0530

    Created function to search a BST

--- index.c
+++ index.c
@@ -1,157 +1,82 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 
-typedef struct Node {
+typedef struct node {
+	struct node *left;
+	struct node *right;
 	int data;
-	struct Node *left;
-	struct Node *right;
 } Node;
-Node* insertInTree(Node *root,int data);
 
 
-Node* createNode(int data) {
+Node *createNode(int data) {
 	Node *root = (Node *)malloc(sizeof(Node));
-	root->data = data;
 	root->left = NULL;
 	root->right = NULL;
+	root->data = data;
 	return root;
 }
 
-Node* createTree(int data) {
-	Node *root = createNode(data);
-	// srand(time(NULL));
-	// for (int i = 0; i < 10; ++i) {
-	// 	int r = rand()%100;
-	// 	insertInTree(root, r);
-	// }
-
-	insertInTree(root, 10);
-	insertInTree(root, 5);
-	insertInTree(root, 17);
-	insertInTree(root, 1);
-	insertInTree(root, 8);
-
-	insertInTree(root, 30);
-	insertInTree(root, 25);
-	insertInTree(root, 40);
-	insertInTree(root, 22);
-	insertInTree(root, 27);
-	insertInTree(root, 35);
-	insertInTree(root, 50);
-
-	return root;
+Node* createTree(int rootValue) {
+	return createNode(rootValue);
 }
 
-Node* insertInTree(Node *root,int data) {
+Node* insert(Node *root ,int value) {
 	if (!root) {
-		return createNode(data);
+		return createNode(value);
 	}
-
-	if (data < root->data) {
-		root->left = insertInTree(root->left, data);
+	if (root->data > value) {
+		root->left = insert(root->left, value);
 	}
 	else {
-		root->right = insertInTree(root->right, data);
+		root->right = insert(root->right, value);
 	}
-
 	return root;
-
 }
+
 
 void inorderTraversal(Node *root) {
-	if (root) {
-		// printf("%d -> ", root->data);
-		inorderTraversal(root->left);
-		printf("%d -> ", root->data);
-		inorderTraversal(root->right);
-	}
-}
-
-// Queue implementation
-
-typedef struct queue {
-	Node *array[400];
-	int capacity;
-	int end;
-	int front;
-
-}Queue;
-
-Queue* createQueue(int capacity) {
-	Queue *q = (Queue *)malloc(sizeof(Queue));
-	q->capacity = capacity;
-	// q->array = (Node *)malloc(sizeof(Node)*2*capacity);
-	q->end = -1;
-	q->front = 0;
-	return q;
-}
-
-void enqueue(Node *data, Queue *q) {
-	// printf("Queuing - %d\n", data->data);
-	q->end++;
-	q->array[q->end] = data;
-}
-
-void dequeue(Queue *q) {
-
-
-	if (q->front <= q->end) {
-		printf("Dequeued %d\n", q->array[q->front]->data);
-		q->front++;
-	}
-
-
-}
-
-void printQueue(Queue *q) {
-	printf("Front %d End %d\n",q->front, q->end);
-	for (int i = q->front; i <= q->end; i++) {
-		printf("%d - ", q->array[i]->data);
-	}
-	printf("\n");
-}
-
-Queue *globalQueue;
-
-void BFS(Node *root) {
 	if (!root) {
 		return;
 	}
-	if (root->left == NULL && root->right == NULL) {
-		// dequeue(globalQueue);
-		// return;
-	}
-
-	if (root->left) {
-		enqueue(root->left, globalQueue);
-	}
-
-	if (root->right) {
-		enqueue(root->right, globalQueue);
-	}
-	// printQueue(globalQueue);
-	dequeue(globalQueue);
-	// printQueue(globalQueue);
-	BFS(globalQueue->array[globalQueue->front]);
-	
+	inorderTraversal(root->left);
+	printf("%d->", root->data);
+	inorderTraversal(root->right);
 
 }
 
+void search(Node *root, int data) {	
+	if (!root) {
+		return;
+	}
+
+	if (root->data == data) {
+		printf("Found it! \n");
+		return;
+	}
+
+	if (root->data > data) {
+		printf("Going left!\n");
+		search(root->left, data);
+	}
+	else {
+		printf("Going right!\n");
+		search(root->right, data);
+	}
+
+}
+
+
 int main(void) {
-	Node *root = createTree(20);
+	Node *root = createTree(50);
+	insert(root, 20);
+	insert(root, 40);
+	insert(root, 21);
+	insert(root, 22);
+	insert(root, 42);
+	insert(root, 44);
+
 	inorderTraversal(root);
 	printf("\n");
-	globalQueue = createQueue(20);
-	// printQueue(globalQueue);
-	enqueue(root, globalQueue);
-	BFS(root);
-	// Queue *q = createQueue(20);
-	// enqueue(201,q);
-	// enqueue(202,q);
-	// enqueue(203,q);
-	// enqueue(204,q);
-	// enqueue(205,q);
-	// printQueue(q);
-
+	search(root, 44);
+	return 0;
 }
