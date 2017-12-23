2519869c96cdb028a331979c4f175c2fd2f33360
Aamir Anwar <aamir.anwar021193@gmail.com>
2017-12-23 11:10:19 +0530

    Rewrote Binary tree operations with Morris traversal

--- index.c
+++ index.c
@@ -1,82 +1,170 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
+// Binary Tree Node Structure
 typedef struct node {
 	struct node *left;
 	struct node *right;
 	int data;
-} Node;
+}Node;
 
 
-Node *createNode(int data) {
-	Node *root = (Node *)malloc(sizeof(Node));
-	root->left = NULL;
-	root->right = NULL;
-	root->data = data;
-	return root;
+// Function Prototypes
+Node* createBinaryTree(int *values, int size);
+Node* createNode(int data);
+bool searchBinaryTree(int data, Node *root);
+Node* insertIntoBinaryTree(int data, Node *root);
+void preorder(Node *root);
+void inorder(Node *root);
+void postorder(Node *root);
+void createVine(Node **root);
+void printVine(Node *root);
+Node* deleteInorderPredecessor(Node *root);
+
+// Function definitions 
+Node* createNode(int data) {
+	Node *newNode = (Node *)malloc(sizeof(Node));
+	newNode->left = NULL;
+	newNode->right = NULL;
+	newNode->data = data;
+	return newNode;
 }
-
-Node* createTree(int rootValue) {
-	return createNode(rootValue);
-}
-
-Node* insert(Node *root ,int value) {
-	if (!root) {
-		return createNode(value);
-	}
-	if (root->data > value) {
-		root->left = insert(root->left, value);
-	}
-	else {
-		root->right = insert(root->right, value);
-	}
-	return root;
-}
-
-
-void inorderTraversal(Node *root) {
-	if (!root) {
-		return;
-	}
-	inorderTraversal(root->left);
-	printf("%d->", root->data);
-	inorderTraversal(root->right);
-
-}
-
-void search(Node *root, int data) {	
-	if (!root) {
-		return;
+Node* createBinaryTree(int *values, int size) {
+	if (size <= 0) {
+		printf("No values in the input!\n");
 	}
 
-	if (root->data == data) {
-		printf("Found it! \n");
-		return;
+	Node *root = createNode(values[0]);
+	printf("Inserting %d \n", values[0]);
+	for (int i = 1; i < size; ++i) {
+		printf("Inserting %d ", values[i]);
+		insertIntoBinaryTree(values[i],root);
+		printf("Done!!\n");
 	}
 
-	if (root->data > data) {
-		printf("Going left!\n");
-		search(root->left, data);
-	}
-	else {
-		printf("Going right!\n");
-		search(root->right, data);
-	}
-
-}
-
-
-int main(void) {
-	Node *root = createTree(50);
-	insert(root, 20);
-	insert(root, 40);
-	insert(root, 21);
-	insert(root, 22);
-	insert(root, 42);
-	insert(root, 44);
-
-	inorderTraversal(root);
+	printf("Done Creating Tree!\n");
 	printf("\n");
-	search(root, 44);
+	return root;
+}
+
+Node* insertIntoBinaryTree(int data, Node *root) {
+	if (root == NULL) {
+		return createNode(data);
+	}
+	if (data < root->data) {
+		root->left = insertIntoBinaryTree(data, root->left);
+	}
+	else {
+		root->right = insertIntoBinaryTree(data, root->right);	
+	}
+
+	return root;
+}
+
+void preorder(Node *root) {
+	if (root == NULL) {
+		return;
+	}
+	printf("%d ", root->data);
+	preorder(root->left);
+	preorder(root->right);
+}
+
+void inorder(Node *root) {
+	if (root == NULL) {
+		return;
+	}
+	inorder(root->left);
+	printf("%d ", root->data);
+	inorder(root->right);
+}
+
+void postorder(Node *root) {
+	if (root == NULL) {
+		return;
+	}
+	postorder(root->left);
+	postorder(root->right);
+	printf("%d ", root->data);	
+}
+
+bool searchBinaryTree(int data, Node *root) {
+	if (root == NULL ) {
+		return false;
+	}
+
+	if (data < root->data) {
+		return searchBinaryTree(data, root->left);
+	}
+	else if (data > root->data) {
+		return searchBinaryTree(data, root->right);
+	}
+	return true;
+}
+
+Node* deleteInorderPredecessor(Node *root) {
+	if (!root) {return NULL;}
+	
+	Node *temp = root;	
+	root = root->left;
+
+	if (root->right) {
+		temp = root;
+		root = root->right;
+	}
+	else {
+		temp->left = root->left;
+		return root;
+	}
+
+	while(root->right) {
+		root = root->right;
+		temp = temp->right;
+	}
+	temp->right = root->left;
+	return root;
+
+}
+
+void createVine(Node **root) {
+	if ((*root) == NULL || (*root)->left == NULL) {
+		return;
+	}
+	while ((*root)->left) {
+		printf("Next one on the left of %d is %d \n", (*root)->data,(*root)->left->data);
+		Node *pre = deleteInorderPredecessor((*root));
+		printf("Preorder predecessor is %d\n", pre->data);
+		pre->right = *root;
+		pre->left = (*root)->left;
+		*root = pre;
+	}
+	createVine(&((*root)->right));
+}
+
+void printVine(Node *root) {
+	while(root) {
+		printf("%d ", root->data);
+		root = root->right;
+	}
+
+	printf("\n");
+}
+
+// Main 
+int main(void) {
+	int data[] = {100, 90, 80, 70, 60, 50, 40, 30, 20, 10};
+	int size = sizeof(data)/sizeof(data[0]);
+	printf("Hi there! i have %d elements\n", size);
+	Node *root = createBinaryTree(data, size);
+	inorder(root);
+	printf("\n");
+
+	// Morris Traversal
+	createVine(&root);
+	printf("Printing vine!\n");
+	printVine(root);
+
+
 	return 0;
 }
