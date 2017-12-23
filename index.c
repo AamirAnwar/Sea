6331fd295a112d4dba6e7d58a5d3472c5aa77641
Aamir Anwar <aamir.anwar021193@gmail.com>
2017-12-23 23:21:42 +0530

    First working version of the DSW algorithm in C

--- index.c
+++ index.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <math.h>
 
 // Binary Tree Node Structure
 typedef struct node {
@@ -21,8 +22,13 @@ void postorder(Node *root);
 void createVine(Node **root);
 void printVine(Node *root);
 Node* deleteInorderPredecessor(Node *root);
+int numberOfNodes(Node *root);
+Node* DSW(Node **root);
+Node* rotateLeft(Node *root);
+Node* rotateRight(Node *root);
 
 // Function definitions 
+
 Node* createNode(int data) {
 	Node *newNode = (Node *)malloc(sizeof(Node));
 	newNode->left = NULL;
@@ -36,11 +42,11 @@ Node* createBinaryTree(int *values, int size) {
 	}
 
 	Node *root = createNode(values[0]);
-	printf("Inserting %d \n", values[0]);
+	// printf("Inserting %d \n", values[0]);
 	for (int i = 1; i < size; ++i) {
-		printf("Inserting %d ", values[i]);
+		// printf("Inserting %d ", values[i]);
 		insertIntoBinaryTree(values[i],root);
-		printf("Done!!\n");
+		// printf("Done!!\n");
 	}
 
 	printf("Done Creating Tree!\n");
@@ -105,9 +111,9 @@ bool searchBinaryTree(int data, Node *root) {
 
 Node* deleteInorderPredecessor(Node *root) {
 	if (!root) {return NULL;}
-	
 	Node *temp = root;	
 	root = root->left;
+	if (!root) {return temp;}
 
 	if (root->right) {
 		temp = root;
@@ -128,17 +134,21 @@ Node* deleteInorderPredecessor(Node *root) {
 }
 
 void createVine(Node **root) {
-	if ((*root) == NULL || (*root)->left == NULL) {
+	if ((*root) == NULL) {
 		return;
 	}
 	while ((*root)->left) {
-		printf("Next one on the left of %d is %d \n", (*root)->data,(*root)->left->data);
-		Node *pre = deleteInorderPredecessor((*root));
-		printf("Preorder predecessor is %d\n", pre->data);
+		// printf("Next one on the left of %d is %d \n", (*root)->data,(*root)->left->data);
+		Node *temp = *root;
+		Node *pre = deleteInorderPredecessor(temp);
+		// printf("Preorder predecessor is %d\n", pre->data);
 		pre->right = *root;
 		pre->left = (*root)->left;
+		(*root)->left = NULL;
 		*root = pre;
 	}
+	if ((*root)->right == NULL) {return;} 	
+	// printf("Next one to be stripped of left children is %d \n",(*root)->right->data);
 	createVine(&((*root)->right));
 }
 
@@ -151,19 +161,104 @@ void printVine(Node *root) {
 	printf("\n");
 }
 
+int numberOfNodes(Node *root) {
+	if (root == NULL) {
+		return 0;
+	}
+	return 1 + numberOfNodes(root->left) + numberOfNodes(root->right);
+}
+
+Node* rotateLeft(Node *root) {
+	if (root == NULL || root->right == NULL) {
+		return root;
+	}
+	Node *nodeToRotate = root->right;
+	root->right = nodeToRotate->left;
+	nodeToRotate->left = root;
+	return nodeToRotate;
+}
+
+Node* rotateRight(Node *root) {
+	if (root == NULL) {
+		return root;
+	}
+	exit(0);
+}
+
+
+Node* DSW(Node **root) {
+	// Create backbone tree
+	if (*root == NULL) {
+		return *root;
+	}
+
+	createVine(root);
+	printVine(*root);
+
+	int n = numberOfNodes(*root);
+	int m = pow(2, floor(log2(n+1))) - 1;
+	
+	printf("There are %d in the closest perfect tree form but we have %d nodes here!\n", m,n);
+
+	if (n > m) {
+		// Perform n-m rotations from the top (Pre-processing)
+		for (int i=0;i<(n-m);i++) {
+			*root = rotateLeft(*root);
+		}
+	}
+
+	// Perform a rotation for every alternate node
+
+	while (m > 1) {
+		Node *temp = *root;
+		Node *parent = NULL;
+		*root = temp->right;
+		printf("Root is now %d\n",(*root)->data );
+		m = m/2;
+		printf("M is now %d\n", m);
+		printVine(temp);
+		while (temp && temp->right && (temp->right->right || temp->right->left)) {
+			printf("Rotating about node %d \n", temp->data);
+			printVine(*root);
+			printf("--------------\n");
+			if (parent) {
+				parent->right = temp->right;
+			}
+			temp = rotateLeft(temp);
+			parent = temp;
+			temp = temp->right;
+
+		}
+
+	}
+	return *root;
+
+}
+
 // Main 
 int main(void) {
-	int data[] = {100, 90, 80, 70, 60, 50, 40, 30, 20, 10};
+	int data[] = {50,30,100,10,40, 110,75};
 	int size = sizeof(data)/sizeof(data[0]);
 	printf("Hi there! i have %d elements\n", size);
 	Node *root = createBinaryTree(data, size);
-	inorder(root);
+	// inorder(root);
+	postorder(root);
 	printf("\n");
 
+	// printf("Rotating root\n");
+	// root = rotateLeft(root);
+	// postorder(root);
+	// printf("\n");
+	// printf("Total number of nodes is %d\n", numberOfNodes(root));
+
 	// Morris Traversal
-	createVine(&root);
-	printf("Printing vine!\n");
-	printVine(root);
+	// createVine(&root);
+	// printf("Printing vine!\n");
+	// printVine(root);
+
+	root = DSW(&root);
+	postorder(root);
+
 
 
 	return 0;
