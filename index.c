b890be8471fc54c59e6f6984c2ea64a3d91c1087
Aamir Anwar <aamir.anwar021193@gmail.com>
2018-01-01 13:30:06 +0530

    Reverted index.c to barebones

--- index.c
+++ index.c
@@ -1,42 +1,6 @@
-// TODO - Change to .h file
-#include "binarytree_DSW_Morris.c"
-#include <limits.h>
-int *counter;
-
-// This algorithm runs in O(n) time.
-Node* createTreeFromInorder(int *traversal, int size, int min, int max, Node *root) {
-
-	if ((*counter) >= size) {
-		return NULL;
-	}
-
-	int data = traversal[*counter];
-
-	printf("Data is %d\n", data);
-	printf("Min is %d and Max is %d\n", min, max);
-	if (data >= min && data <= max && root == NULL) {
-		printf("Found the place for this bugger! %d\n", data);
-		*counter = (*counter) + 1;
-		root = createNode(data);
-	}
-
-	if (root) {
-		root->left = createTreeFromInorder(traversal, size, min,data, root->left);
-		root->right = createTreeFromInorder(traversal, size, data, max,root->right);
-	}
-	return root;
-
-}
+#include <stdio.h>
+#include <stdlib.h>
 
 int main(void) {
-	int data[] = {50,20,10,30,70,60,100};
-	int size = sizeof(data)/sizeof(data[0]);
-	int initialValue = 0;
-	counter = &initialValue;
-	// Node *root = insertIntoBinaryTree(data[0], NULL);
-	Node *root = NULL;
-	root = createTreeFromInorder(data, size,-INT_MAX, INT_MAX, root);
-	inorder(root);
-
 	return 0;
 }
